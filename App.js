247b7c1229985e6cbf8edfbbfdd843746a490655
Paulo Henrique Rodrigues <paulohenriquerodrigues@outlook.com>
2025-07-28 17:30:04 +0000

    versao_inicial

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+import HomePage from './src/pages/HomePage'
+
+const App = () => {
+
+  return (
+    <HomePage />
+  );
+}
+
+export default App;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const HomePage = () => {
+
+  return (
+    <View style={{flex:1, AlignItems: 'center', JustifyContent:'center'}}>
+      <Text> Hello World! </Text>
+    </View>
+  );
+}
+
+export default HomePage;
